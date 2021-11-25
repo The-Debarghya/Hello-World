de5084c0fea8ceeb693639d2e45d958b2df2b916
Rohit Sadhu <rohitsadhupersonal@gmail.com>
2021-11-25 22:42:29 +0530

    added hello.c 25-11-2021

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!!!");
+    return 0;
+}
